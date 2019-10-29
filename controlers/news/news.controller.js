6e50d0b655694e08d8f55f4d47497080e7867283
Meromen <meromen1375@gmail.com>
2019-10-29 14:43:48 +1000

    Fix bug

--- controlers/news/news.controller.js
+++ controlers/news/news.controller.js
@@ -1,12 +1,10 @@
-const redisClient = require("../../_helpers/redis");
+const redis = require("../../_helpers/redis");
 
 function getNews(req, res) {
-  redisClient.get("NEWS", (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.json(JSON.parse(data));
-  });
+  redis
+    .getNews("NEWS")
+    .then(data => res.json(JSON.parse(data)))
+    .catch(err => res.status(500).send(err));
 }
 
 module.exports = {
